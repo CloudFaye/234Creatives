cf6bdea3610a56bdd1023407bd4f5ea66c4d28d9
Faye seun <samuelfaye96@gmail.com>
2025-02-18 02:01:58 +0100

    fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -133,7 +133,7 @@ import { browser } from '$app/environment';
 <div class="home">
     {#if browser}
     {#if showLoader} // only enter dom if showLoader is true
-       <div transition:slide={{duration: 1000, delay: 0, easing:cubicOut, axis: 'y'}} class="loader">
+       <div transition:slide={{duration: 60*1000, delay: 0, easing:cubicOut, axis: 'y'}} class="loader">
             <Square size="38" color="#FF3E00" unit="px" duration="10s" />
             <p class='mx-2'>Getting all the good guys for you</p>
         </div> 
